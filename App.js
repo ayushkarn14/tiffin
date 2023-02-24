34a4edb3b8295ecbb816801e1770a0bd37c4b179
ayushkarn14 <ayushkarn14@gmail.com>
2023-02-24 15:11:54 +0530

    refactoring

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-// import LoginAndSignUpScreen from './screens/LoginAndSignUpScreen';
-// import {AsyncStorage} from 'react-native';
-// import HomeScreen from './screens/HomeScreen';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
-// import React from 'react';
-
-// const Stack = createStackNavigator();
-
-// export default function App() {
-//   const [data,setData]=React.useState(null);
-//   async function fun(){
-//     const my_num = await AsyncStorage.getItem('number');
-//     console.log(my_num);
-//     if(my_num!=null)
-//       setData(my_num);
-//   }
-//   fun();
-//     // if(data==null)
-//     //   return(<LoginAndSignUpScreen/>);
-//     // else
-//     //   return(<HomeScreen number={data}/>);
-
-// }
-
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 import { NavigationContainer } from "@react-navigation/native";
